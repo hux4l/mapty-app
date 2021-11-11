75b6261599a4688fe3ac21f37228468acc9d72e3
tobas <tomas.baska@outlook.com>
2021-11-11 16:23:37 +0100

    prettier settings
    Took 8 minutes

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 'use strict';
 
-
 // prettier-ignore
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
@@ -13,45 +12,45 @@ const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
 class Workout {
-    date = new Date();
-    id = (Date.now() + '').slice(-10);
+  date = new Date();
+  id = (Date.now() + '').slice(-10);
 
-    constructor(coords, distance, duration) {
-        this.coords =  coords;
-        this.distance = distance;   // km
-        this.duration = duration;   // min
-    }
+  constructor(coords, distance, duration) {
+    this.coords = coords;
+    this.distance = distance; // km
+    this.duration = duration; // min
+  }
 }
 
 // running extends Workout class
 class Running extends Workout {
-    constructor(coords, distance, duration, cadence) {
-        super(coords, distance, duration);
-        this.cadence = cadence;
-        this.calcPace();
-    }
+  constructor(coords, distance, duration, cadence) {
+    super(coords, distance, duration);
+    this.cadence = cadence;
+    this.calcPace();
+  }
 
-    // calculate pace
-    calcPace() {
-        // min/km
-        this.pace = this.duration / this.distance;
-        return this.pace;
-    }
+  // calculate pace
+  calcPace() {
+    // min/km
+    this.pace = this.duration / this.distance;
+    return this.pace;
+  }
 }
 
 //cycling extends Workout class
 class Cycling extends Workout {
-    constructor(coords, distance, duration, elevationGain) {
-        super(coords, distance, duration);
-        this.elevationGain = elevationGain;
-        this.calcSpeed();
-    }
+  constructor(coords, distance, duration, elevationGain) {
+    super(coords, distance, duration);
+    this.elevationGain = elevationGain;
+    this.calcSpeed();
+  }
 
-    calcSpeed() {
-        // km/h
-        this.speed = this.distance / (this.duration / 60);
-        return this.speed;
-    }
+  calcSpeed() {
+    // km/h
+    this.speed = this.distance / (this.duration / 60);
+    return this.speed;
+  }
 }
 
 // const run1 = new Running([49.1167941, 18.3306917], 5.2, 24, 178);
@@ -63,85 +62,103 @@ class Cycling extends Workout {
 //////////////////////////////
 // Application architecture
 class App {
-    #map;
-    #mapEvent;
+  #map;
+  #mapEvent;
 
-    constructor() {
-        this._getPosition();
-        form.addEventListener('submit', this._newWorkout.bind(this));
-        this._toggleElevationField();
-    }
+  constructor() {
+    this._getPosition();
+    form.addEventListener('submit', this._newWorkout.bind(this));
+    this._toggleElevationField();
+  }
 
-    // gets users current position
-    _getPosition() {
-        if(navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(this._loadMap.bind(this), function () { alert('Could not get your position'); });
+  // gets users current position
+  _getPosition() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        this._loadMap.bind(this),
+        function () {
+          alert('Could not get your position');
         }
+      );
     }
+  }
 
-    // load map on current user location
-    _loadMap(position) {
-        // gets latitude and longitude from API
-        const {latitude} = position.coords;
-        const {longitude} = position.coords;
-        // creates link to current position in google maps
-        console.log(`https://www.google.com/maps/@${latitude},${longitude}z`);
+  // load map on current user location
+  _loadMap(position) {
+    // gets latitude and longitude from API
+    const { latitude } = position.coords;
+    const { longitude } = position.coords;
+    // creates link to current position in google maps
+    console.log(`https://www.google.com/maps/@${latitude},${longitude}z`);
 
-        const coords = [latitude, longitude];
+    const coords = [latitude, longitude];
 
-        console.log(this);
-        this.#map = L.map('map', {closePopupOnClick: false}).setView(coords, 13);
-        // map.closePopupOnClick = false;
-        // console.log(map);
+    console.log(this);
+    this.#map = L.map('map', { closePopupOnClick: false }).setView(coords, 13);
+    // map.closePopupOnClick = false;
+    // console.log(map);
 
-        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            // L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {       // another style theme
-            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        }).addTo(this.#map);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      // L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {       // another style theme
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(this.#map);
 
-        // method from leaflet, handling click on map
-        // to work event handler function we need to bind the handler to self
-        this.#map.on('click', this._showForm.bind(this));
-    }
+    // method from leaflet, handling click on map
+    // to work event handler function we need to bind the handler to self
+    this.#map.on('click', this._showForm.bind(this));
+  }
 
-    // show the input form when user clicks anywhere on the map
-    _showForm(mapE) {
-        // wont work
-        this.#mapEvent = mapE;
-        form.classList.remove('hidden');
-        inputDistance.focus();
-        // console.log(mapEvent);
-    }
+  // show the input form when user clicks anywhere on the map
+  _showForm(mapE) {
+    // wont work
+    this.#mapEvent = mapE;
+    form.classList.remove('hidden');
+    inputDistance.focus();
+    // console.log(mapEvent);
+  }
 
-    // switches fields in input form based on workout type
-    _toggleElevationField() {
-        // adds event listener on changing selector input
-        inputType.addEventListener('change', function () {
-            inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
-            inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
-        });
-    }
+  // switches fields in input form based on workout type
+  _toggleElevationField() {
+    // adds event listener on changing selector input
+    inputType.addEventListener('change', function () {
+      inputElevation
+        .closest('.form__row')
+        .classList.toggle('form__row--hidden');
+      inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+    });
+  }
 
-    // from event listener, display marker
-    _newWorkout(e) {
-        e.preventDefault();
-        // console.log(mapEvent);
+  // from event listener, display marker
+  _newWorkout(e) {
+    e.preventDefault();
+    // console.log(mapEvent);
 
-        // clear input fields
-        inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value = '';
+    // clear input fields
+    inputDistance.value =
+      inputDuration.value =
+      inputCadence.value =
+      inputElevation.value =
+        '';
 
-        // gets coordinates from cursor click position
-        const {lat, lng} = this.#mapEvent.latlng;
+    // gets coordinates from cursor click position
+    const { lat, lng } = this.#mapEvent.latlng;
 
-        // adds marker on cursor position on the map
-        L.marker([lat, lng]).addTo(this.#map).bindPopup(L.popup({
-            maxWidth: 250,
-            minWidth: 150,
-            autoClose: false,
-            closeOnCLick: false,
-            className: 'running-popup',
-        })).setPopupContent('Workout').openPopup();
-    }
+    // adds marker on cursor position on the map
+    L.marker([lat, lng])
+      .addTo(this.#map)
+      .bindPopup(
+        L.popup({
+          maxWidth: 250,
+          minWidth: 150,
+          autoClose: false,
+          closeOnCLick: false,
+          className: 'running-popup',
+        })
+      )
+      .setPopupContent('Workout')
+      .openPopup();
+  }
 }
 
 const app = new App();
